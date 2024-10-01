c16393ca9b74a9c7acd5b3bb3421e15ce2ca1cec
agrmet <agron-metaj@hotmail.com>
2024-10-01 15:19:19 +0200

    Extension advice is done

--- src/sections/Advice/index.jsx
+++ src/sections/Advice/index.jsx
@@ -1,11 +1,39 @@
+import { useState } from "react";
+import AdviceSlip from "./components/AdviceSlip";
+import FavouriteSlipsList from "./components/FavouriteSlipsList"
+
 function AdviceSection() {
+  const [advice, setAdvice] = useState("Always the burrito.");
+  const [favorites, setFavorites] = useState([
+    "Measure twice, cut once.",
+    "Don't let the bastards grind you down.",
+    "Always the burrito."
+  ]);
+
+  const fetchAdvice = async () => {
+    try {
+      const response = await fetch("https://api.adviceslip.com/advice");
+      const data = await response.json();
+      setAdvice(data.slip.advice);
+    } catch (error) {
+      console.error("Error fetching advice:", error);
+    }
+  };
+
+  const saveToFavorites = () => {
+    if (!favorites.includes(advice)) {
+      setFavorites([...favorites, advice]);
+    }
+  };
+
   return (
     <section>
       <h2>Advice Section</h2>
-      <section className="adivce-slip"></section>
-      <section className="favourtite-slips-list"></section>
+      <AdviceSlip advice={advice} fetchAdvice={fetchAdvice} saveToFavorites={saveToFavorites}/>
+      <FavouriteSlipsList favorites={favorites}/>
+      
     </section>
-  )
+  );
 }
 
-export default AdviceSection
+export default AdviceSection;
